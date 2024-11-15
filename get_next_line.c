4127aeba52fbf4a1ae30006c8e1a1ffcf1a1f313
Phuoc Nguyen <phuoc94@hotmail.fi>
2024-11-15 22:55:48 +0200

    add buffer

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,33 @@
 /*   By: phuocngu <phuocngu@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 16:46:51 by phuocngu          #+#    #+#             */
-/*   Updated: 2024/11/15 17:06:18 by phuocngu         ###   ########.fr       */
+/*   Updated: 2024/11/15 22:54:57 by phuocngu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+#include <stdio.h> //printf
+
+
 char *get_next_line(int fd)
 {
+	int line_len;
+	int sz;
+	char *buffer;
+	line_len = 0;
 
+	buffer = malloc((BUFFER_SIZE + 1) * sizeof(*buffer));
+	if(!buffer)
+		return (NULL);
+
+	sz = read(fd, buffer, BUFFER_SIZE);
+	buffer[sz] = '\0';
+
+	while (sz)
+	{
+		printf("buffer: %s \n", buffer);
+		sz = read(fd, buffer, BUFFER_SIZE);
+	}
+
+	return (char *)(0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,28 @@
 /*   By: phuocngu <phuocngu@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 16:47:12 by phuocngu          #+#    #+#             */
-/*   Updated: 2024/11/15 16:50:42 by phuocngu         ###   ########.fr       */
+/*   Updated: 2024/11/15 22:52:47 by phuocngu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 10
+#endif
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# include <stdlib.h> // for malloc
+# include <fcntl.h> //for open file
+# include <unistd.h> // read
+
 char *get_next_line(int fd);
+
+typedef struct s_list
+{
+	char			*content;
+	int				len;
+	struct s_list	*next;
+}					t_list;
 
 #endif
